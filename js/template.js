457052087d2998acc90f001f9ad155d162e88dcc
Selman N. Tabet <33247400+selmantabet@users.noreply.github.com>
2022-11-12 15:19:37 +0000

    Added files.

---
+++ js/template.js
@@ -0,0 +1,54 @@
+const fs = require('fs');
+
+module.exports = {
+
+    // Exercise 1 - Iris Species Classifier
+    exercise1: (SepalLen, SepalWid, PetalLen, PetalWid) => {
+        return undefined;
+    },
+
+    // Exercise 2 - Dog Breeds Standards
+    exercise2: (breed, height, weight, male) => {
+        return undefined;
+    },
+
+    // Exercise 3 - Basic Statistics
+    exercise3: (l) => {
+        return undefined;
+    },
+
+    // Exercise 4 - Finite-State Machine Simulator
+    exercise4: (trans, init_state, input_list) => {
+        return undefined;
+    },
+
+    // Exercise 5 - Document Stats
+    exercise5: (filename) => {
+        return undefined;
+    },
+
+    // Exercise 6 - List Depth
+    exercise6: (l) => {
+        return undefined;
+    },
+
+    // Exercise 7 - Change, please
+    exercise7: (amount,coins) => {
+        return undefined;
+    },
+
+    // Exercise 8 - Five Letter Unscramble
+    exercise8: (s) => {
+        return undefined;
+    },
+
+    // Exercise 9 - Wordle Set
+    exercise9: (green,yellow,gray) => {
+        return undefined;
+    },
+
+    // Exercise 10 - One Step of Wordle
+    exercise10: (green,yellow,gray) => {
+        return undefined;
+    },
+}
---
+++ python/template.py
@@ -0,0 +1,61 @@
+import copy
+
+# Exercise 1 - Iris Species Classifier
+
+
+def exercise1(SepalLen, SepalWid, PetalLen, PetalWid):
+    return None
+
+# Exercise 2 - Dog Breeds Standards
+
+
+def exercise2(breed, height, weight, male):
+    return None
+
+# Exercise 3 - Basic Statistics
+
+
+def exercise3(l):
+    return None
+
+# Exercise 4 - Finite-State Machine Simulator
+
+
+def exercise4(trans, init_state, input_list):
+    return None
+
+# Exercise 5 - Document Stats
+
+
+def exercise5(filename):
+    return None
+
+# Exercise 6 - List Depth
+
+
+def exercise6(l):
+    return None
+
+# Exercise 7 - Change, please
+
+
+def exercise7(amount, coins):
+    return None
+
+# Exercise 8 - Five Letter Unscramble
+
+
+def exercise8(s):
+    return None
+
+# Exercise 9 - Wordle Set
+
+
+def exercise9(green, yellow, gray):
+    return None
+
+# Exercise 10 - One Step of Wordle
+
+
+def exercise10(green, yellow, gray):
+    return None
